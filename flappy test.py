42d76fb8b67b677296fc3c5b2c44c6f68e8cef69
Edgar Garza <45373932+edgargarza7@users.noreply.github.com>
2019-10-25 16:17:34 -0500

    Add files via upload

--- flappy test.py
+++ flappy test.py
@@ -1,34 +1,66 @@
 from sense_hat import SenseHat
 from random import randint
+from time import sleep
 
 sense = SenseHat()
+
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+x = 0
+y = 0
 
 def flatten(matrix):
     flattened = [pixel for row in matrix for pixel in row]
     return flattened
 
-#g = (0, 255, 0)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
+def gen_pipes(matrix):
+    for row in matrix:
+        row[-1] = RED
+    gap = randint(1,6)
+    matrix[gap][-1] = BLUE
+    matrix[gap+1][+1] = BLUE
+    matrix[gap-1][-1] = BLUE
+    
+    return matrix
 
-"""creeper_pixels = [
-    g, g, g, g, g, g, g, g,
-    g, g, g, g, g, g, g, g,
-    g, b, b, g, g, b, b, g,
-    g, b, b, g, g, b, b, g,
-    g, g, g, b, b, g, g, g,
-    g, g, b, b, b, b, g, g,
-    g, g, b, b, b, b, g, g,
-    g, g, b, g, g, b, g, g
-]"""
+def move_pipes(matrix):
+    for row in matrix:
+        for i in range(7):
+            row[i] = row[i+1]
+        row[-1] = BLUE
+    return matrix
 
-#sense.set_pixels(creeper_pixels)
+def draw_astronaut(event):
+    global x
+    global y
+    
+    sense.set_pixel(x,y,BLUE)
+    
+    if event.action == "pressed":
+        if event.direction == "up":
+            y = y-1
+        elif event.direction == "down":
+            y = y+1
+        elif event.direction == "right":
+            x+=1
+        elif event.direction == "left":
+            x-=1
+    
+    sense.set_pixel(x,y,YELLOW)
+    
+
+
+
 
 matrix = [[BLUE for column in range(8)] for row in range(8)]
-gap = randint(1,6)
-for row in matrix:
-    row[-1] = RED
 
-print (matrix)
-matrix = flatten(matrix)
-sense.set_pixels(matrix)
+sense.stick.direction_any = draw_astronaut
+
+while True:
+    matrix = gen_pipes(matrix)
+    for i in range(4):
+        sense.set_pixels(flatten(matrix))
+        matrix = move_pipes(matrix)
+        sleep(1)
